ee0b7bf067d2da6f20609fc791416f78f1b9fb33
hu <348968803@qq.com>
2021-08-25 19:05:36 +0800

    项目搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
+  <div id="app">
+    <MyHeader/>
+    <MyGoods/>
+
+    <MyFooter/>
 
   </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader.vue'
+import MyGoods from './components/MyGoods.vue'
+import MyFooter from './components/MyFooter.vue'
+
 export default {
+  components:{
+    MyHeader,
+    MyGoods,
+    
+    MyFooter
+  },
   data () {
     return {
 
@@ -23,5 +37,7 @@ export default {
 </script>
 
 <style scoped>
-
+#app{
+  padding-top: 50px;
+}
 </style>
